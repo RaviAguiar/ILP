e1a0b9ebf9ce35abba85d8b339c574b647b884a5
Ravi Rodrigues <ravi.rarezende@gmail.com>
2026-04-09 13:29:00 +0000

    feitas questões 11-14

--- Lista 04/11.py
+++ Lista 04/11.py
@@ -3,3 +3,8 @@ B = 10
 C = 4
 D = True
 E = False
+F = 0
+G = -3
+I = [1, 2, 3, 4, 5]
+print(bool(F))
+print(bool(A))
--- Lista 04/13.py
+++ Lista 04/13.py
@@ -3,3 +3,7 @@ B = 10
 C = 4
 D = True
 E = False
+F = 0
+G = -3
+I = [1, 2, 3, 4, 5]
+print(G < 0 and G > -10)
