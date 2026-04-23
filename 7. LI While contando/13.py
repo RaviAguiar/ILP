861763f069dad2b6c7c872805185d5dda294051d
Ravi Rodrigues <ravi.rarezende@gmail.com>
2026-04-23 13:29:02 +0000

    feita questão 13 e iniciei 14

---
+++ 7. LI While contando/13.py
@@ -0,0 +1,8 @@
+n = -1
+while n < 0:
+    n = int(input('Digite um número inteiro positivo: '))
+
+contador = 3
+while contador <= n:
+    print(contador)
+    contador +=3
---
+++ 7. LI While contando/14.py
@@ -0,0 +1,7 @@
+base = int(input('Digite a base da operação: '))
+
+expoente = -1
+while expoente < 0:
+    expoente = int(input('Digite o expoente positivo: '))
+
+contador = 
